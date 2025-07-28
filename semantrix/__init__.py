3d7e52ddc3afe30554cacb4a455f0816f06a3667
Raj Datta Manohar <rajdattasunny@gmail.com>
2025-07-28 23:08:44 +0530

    Base structure with readme and license

---
+++ semantrix/__init__.py
@@ -0,0 +1,2 @@
+from .core.cache import Semantrix
+from .utils.resource_limits import ResourceLimits 
---
+++ semantrix/vector_store/vector_store.py
@@ -0,0 +1,6 @@
+# Placeholder for FAISSVectorStore class
+class FAISSVectorStore:
+    def search(self, embedding, similarity_threshold=None, top_k=None):
+        pass
+    def add(self, embedding, response):
+        pass 
